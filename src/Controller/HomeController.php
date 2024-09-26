506816c328eae577088e53b72ac87a6b5ffa7b9b
Ghofran <gh.malket@gmail.com>
2024-09-27 00:53:48 +0200

    make "laissez un commentaire" Form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,20 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Opinion;
+use App\Form\OpinionType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    private $request;
+
     public function __construct(
         private ZooController $zooController,
         private HabitatsController $habitatsController,
         private AnimalController $animalController,
         private ServiceController $serviceController,
         private SliderController $sliderController,
-        private OpinionController $opinionController,)
+        private OpinionController $opinionController,
+        private EntityManagerInterface $entityManager,
+        RequestStack $requestStack,)
     {
+        $this->request = $requestStack->getCurrentRequest();
     }
 
     #[Route('/home', name: 'app_home')]
@@ -28,6 +38,27 @@ class HomeController extends AbstractController
         $animaux = $this->animalController->animaux_in_home_page();
         $opinions = $this->opinionController->opinions_in_home_page();
 
+        // Create a new Opinion entity
+        $opinion = new Opinion();
+
+        // Create the form
+        $form = $this->createForm(OpinionType::class, $opinion);
+        $form->handleRequest($this->request);
+
+        // Handle form submission
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Set created_at manually if needed
+            $opinion->setCreatedAt(new \DateTimeImmutable());
+
+            // Persist and flush the entity
+            $this->entityManager->persist($opinion);
+            $this->entityManager->flush();
+
+            // Redirect after form submission to avoid form resubmission issues
+            return $this->redirectToRoute('app_home');
+        }
+
+        // If form is not submitted or is invalid, render the form
         return $this->render('home.html.twig', [
             'zooInfo' => $zooInfo,
             'sliders' => $sliders,
@@ -35,6 +66,7 @@ class HomeController extends AbstractController
             'services' => $services,
             'animaux' => $animaux,
             'opinions' => $opinions,
+            'form' => $form->createView(),
         ]);
     }
 
--- src/Controller/OpinionController.php
+++ src/Controller/OpinionController.php
@@ -2,15 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\Opinion;
+use App\Form\OpinionType;
 use App\Repository\OpinionRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OpinionController extends AbstractController
 {
     public function __construct(
-        private OpinionRepository $repository,)
+        private OpinionRepository $repository)
     {
     }
 
@@ -18,4 +23,5 @@ class OpinionController extends AbstractController
     {
         return $this->repository->findBy([], null, 2);
     }
+
 }
