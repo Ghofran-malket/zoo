268785aaa67df2009edc06ddb5141a90af4ced10
Ghofran <gh.malket@gmail.com>
2024-09-30 13:00:31 +0200

    make Service crud and let the admin only access to create, edit and delete the services

--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -2,11 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Service;
+use App\Form\ServiceType;
 use App\Repository\ServiceRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+#[Route('/services', name: 'app_services')]
 class ServiceController extends AbstractController
 {
     public function __construct(
@@ -21,16 +28,80 @@ class ServiceController extends AbstractController
         return $this->repository->findBy([], null, 4);
     }
 
-    #[Route('/services', name: 'app_services')]
+    //show services page
+    #[Route('/', name: '_show')]
     public function services(): Response
     {
         $zooInfo = $this->zooController->index();
         $sliders = $this->sliderController->sliders_in_home_page();
         $services= $this->repository->findAll();
-        return $this->render('services.html.twig', [
+        return $this->render('services/show.html.twig', [
             'zooInfo' => $zooInfo,
             'sliders' => $sliders,
             'services' => $services,
         ]);
+    }
+
+    #[IsGranted("ROLE_ADMIN")]
+    #[Route('/admin/create', name: '_create')]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $service = new Service();
+        $zooInfo = $this->zooController->index();
+        $sliders = $this->sliderController->sliders_in_home_page();
+        $form = $this->createForm(ServiceType::class, $service);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $service->setCreatedAt(new \DateTimeImmutable());
+            $em->persist($service);
+            $em->flush();
+
+            return $this->redirectToRoute('app_services_show');
+        }
+
+        return $this->render('services/new.html.twig', [
+            'service' => $service,
+            'form' => $form->createView(),
+            'zooInfo' => $zooInfo,
+            'sliders' => $sliders,
+        ]);
+    }
+
+    #[IsGranted("ROLE_ADMIN")]
+    #[Route('/admin/edit/{id}', name: '_edit')]
+    //methods={"GET", "POST"})]
+    public function edit(Request $request, Service $service, EntityManagerInterface $em): Response
+    {
+        $zooInfo = $this->zooController->index();
+        $sliders = $this->sliderController->sliders_in_home_page();
+        $form = $this->createForm(ServiceType::class, $service);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $service->setUpdatedAt(new \DateTimeImmutable());
+            $em->flush();
+
+            return $this->redirectToRoute('app_services_show');
+        }
+
+        return $this->render('services/edit.html.twig', [
+            'service' => $service,
+            'form' => $form->createView(),
+            'zooInfo' => $zooInfo,
+            'sliders' => $sliders,
+        ]);
+    }
+
+    #[IsGranted("ROLE_ADMIN")]
+    #[Route('/admin/delete/{id}', name: '_delete')]
+    public function delete(Request $request, Service $service, EntityManagerInterface $em): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$service->getId(), $request->request->get('_token'))) {
+            $em->remove($service);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('app_services_show');
     }
 }
